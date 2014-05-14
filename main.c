09a6c6cc87b1a57d202c53e31e8b25c62fd91937
ianrtaylor <taylor.ian611@gmail.com>
2014-05-14 09:02:22 -0700

    Add argument parsing.
    Might need to update the makefile in order to link any of the new library headers.

--- main.c
+++ main.c
@@ -1,11 +1,64 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <ncurses.h>
+#include <unistd.h>
 #include "display.h"
 #include "mine.h"
 
+// help info strings:
+static char* usage =
+	"Ncurses Minesweeper by Ian Taylor\n"
+	"usage: ncmine [-hvc] [SIZE]\n";
+	
+static char* help  =
+	"flags:\n"
+	"\t-h : print this information\n"
+	"\t-v : print version information\n"
+	"\t-c : disable color output\n"
+	"[SIZE]:"
+	"\tThe board size as an integer. Defaults to 10.\n";
+
+static char* version =
+	"Ncurses Minesweeper: 0.2";
+
+// runtime options:
+static bool color_output = true; // defaults to true, display.c will disable color output if unsupported.
+static int size = 10;
+
+static void parse_args (int argc, char** argv)
+{
+	char* size_value = NULL;
+	int c;
+	
+	opterr = 0;
+	
+	while ((c = getopt(argc, argv, "hvc")) != -1) {
+		switch (c) {
+		case 'h':
+			printf("%s\n%s", usage, help);
+			exit(0);
+		case 'v':
+			printf("%s\n", version);
+			exit(0);
+		case 'c':
+			color_output = false;
+			break;
+		case '?':
+			if (isprint(optopt))
+				fprintf(stderr, "Unknown option '-%c'.\n", optopt);
+			else
+				fprintf(stderr, "Unknown option character '\\x%x'.\n", optopt);
+			exit(1);
+		default:
+			abort();
+		}
+	}
+}
+
 int main (int argc, char** argv)
 {
-	int size = 10;
 	Mine **board = create_board(size, .15);
 
 	startd(size);
